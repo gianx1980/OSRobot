2b7388e5dd8199d68819293dc7362a3e1c7d6522
gianx1980 <gianx1980@DESKTOP-SFEB6D8>
2025-08-28 22:41:50 +0200

    OSRobot.Tests updated.

--- OSRobot.Tests/TestPlugins/TestPingTask.cs
+++ OSRobot.Tests/TestPlugins/TestPingTask.cs
@@ -10,7 +10,7 @@ namespace OSRobot.Tests.TestPlugins;
 public sealed class TestPingTask
 {
     [TestMethod]
-    public void TestPlugin()
+    public void TestLocalAddress()
     {
         // ---------
         // Arrange
@@ -52,5 +52,50 @@ public sealed class TestPingTask
         ExecResult execResult = result.ExecResults[0];
         float thresholdSuccessRate = (float)execResult.Data["ThresholdSuccessRate"];
         Assert.IsTrue(thresholdSuccessRate == 100, "ThresholdSuccessRate is not equal to 100");
+    }
+
+    [TestMethod]
+    public void TestNonExistentAddress()
+    {
+        // ---------
+        // Arrange
+        // ---------
+        Folder folder = Common.CreateRootFolder();
+
+        PingTaskConfig config = new()
+        {
+            Id = 1,
+            Host = "192.168.5.5",   // Hope it doesn't exist!
+            Attempts = 4,
+            Timeout = 1000
+        };
+
+        PingTask task = new()
+        {
+            ParentFolder = folder,
+            Config = config
+        };
+
+        Common.ConfigureLogPath();
+        (DynamicDataChain dataChain,
+         DynamicDataSet dynamicDataSet,
+         IPluginInstanceLogger logger) = Common.GetTaskDefaultParameters(task);
+
+        // ---------
+        // Act
+        // ---------
+        task.Init();
+        InstanceExecResult result = task.Run(dataChain, dynamicDataSet, 0, logger);
+        task.Destroy();
+
+        // ---------
+        // Assert
+        // ---------
+        Assert.IsTrue(result.ExecResults.Count > 0, "There are no executions.");
+
+        // We are trying to ping a non existent address, 0% success expected
+        ExecResult execResult = result.ExecResults[0];
+        float thresholdSuccessRate = (float)execResult.Data["ThresholdSuccessRate"];
+        Assert.IsTrue(thresholdSuccessRate == 0, "ThresholdSuccessRate is not equal to 0");
     }
 }
