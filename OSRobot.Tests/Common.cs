1d467d4d00fc045d6942422aa713b3632c882b8d
gianx1980 <gianx1980@DESKTOP-SFEB6D8>
2025-08-30 08:20:31 +0200

    Added TestDiskSpaceEvent.

--- OSRobot.Tests/Common.cs
+++ OSRobot.Tests/Common.cs
@@ -33,4 +33,14 @@ internal static class Common
     {
         PluginInstanceLogger.LogPath = @"Logs\";
     }
+
+    public static object? CallPrivateMethod(object o, string methodName, params object[] args)
+    {
+        var mi = o.GetType().GetMethod(methodName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        if (mi != null)
+        {
+            return mi.Invoke(o, args);
+        }
+        return null;
+    }
 }
---
+++ OSRobot.Tests/TestPlugins/TestDiskSpaceEvent.cs
@@ -0,0 +1,179 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OSRobot.Server.Core;
+using OSRobot.Server.Plugins.DiskSpaceEvent;
+
+namespace OSRobot.Tests.TestPlugins;
+
+[TestClass]
+public sealed class TestDiskSpaceEvent
+{
+    [TestMethod]
+    public void TestThresholdToBytesMegaBytes()
+    {
+        // 549755813888: 512GB space
+        // 99999999 is the max value the user can insert in the configuration window
+        DiskSpaceEvent eventObj = new();
+        long? result = (long?)Common.CallPrivateMethod(eventObj, "ThresholdToBytes", [(long)549755813888, (int)99999999, DiskThresholdUnitMeasure.Megabytes]);
+        Assert.IsNotNull(result, "Result is null");
+        Assert.AreEqual(result, 99999999L * 1024L * 1024L);
+    }
+
+    [TestMethod]
+    public void TestThresholdToBytesGigaBytes()
+    {
+        // 99999999 is the max value the user can insert in the configuration window
+        DiskSpaceEvent eventObj = new();
+        long? result = (long?)Common.CallPrivateMethod(eventObj, "ThresholdToBytes", [(long)549755813888, (int)99999999, DiskThresholdUnitMeasure.Gigabytes]);
+
+        Assert.IsNotNull(result, "Result is null");
+        Assert.AreEqual(result, 99999999L * 1024L * 1024L * 1024L);
+    }
+
+    [TestMethod]
+    public void TestThresholdToBytesTeraBytes()
+    {
+        // 999999 is the max value the user can insert in the configuration window
+        DiskSpaceEvent eventObj = new();
+        long? result = (long?)Common.CallPrivateMethod(eventObj, "ThresholdToBytes", [(long)549755813888, (int)999999, DiskThresholdUnitMeasure.Terabytes]);
+
+        Assert.IsNotNull(result, "Result is null");
+        Assert.AreEqual(result, 999999L * 1024L * 1024L * 1024L * 1024L);
+    }
+
+
+    [TestMethod]
+    public void TestSpaceLessThan()
+    {
+        // ---------
+        // Arrange
+        // ---------
+        int checkIntervalEverySeconds = 3;
+        int toleranceSec = 1;
+        Folder folder = Common.CreateRootFolder();
+
+        DiskSpaceEventConfig config = new()
+        {
+            Id = 1,
+            Name = "Disk space event 1",
+            CheckIntervalSeconds = checkIntervalEverySeconds
+        };
+
+        DriveInfo? driveC = DriveInfo.GetDrives().Where(D => D.Name == @"C:\").FirstOrDefault();
+        Assert.IsNotNull(driveC, "Drive C: doesn't exist");
+
+        int diskFreeSpaceToCheckGB = (int)(((double)driveC.AvailableFreeSpace) / 1024 / 1024 / 1024);
+        diskFreeSpaceToCheckGB *= 2;
+
+        DiskThreshold dt = new()
+        {
+            Disk = @"C:\",
+            CheckOperator = CheckOperator.LessThan,
+            ThresholdValue = diskFreeSpaceToCheckGB,
+            UnitMeasure = DiskThresholdUnitMeasure.Gigabytes
+        };
+
+        config.DiskThresholds.Add(dt);
+
+        DiskSpaceEvent eventObj = new()
+        {
+            ParentFolder = folder
+        };
+
+        object objSync = new();
+        ManualResetEvent mre = new(false);
+        bool eventTriggered = false;
+        eventObj.EventTriggered += (sender, e) =>
+        {
+            lock (objSync)
+            {
+                eventTriggered = true;
+            }
+
+            mre.Set();
+        };
+        eventObj.Config = config;
+
+        // ---------
+        // Act
+        // ---------
+        eventObj.Init();
+
+        mre.WaitOne(new TimeSpan(0, 0, checkIntervalEverySeconds + toleranceSec));
+
+        // ---------
+        // Assert
+        // ---------
+        lock (objSync)
+        {
+            Assert.IsTrue(eventTriggered);
+        }
+    }
+
+    [TestMethod]
+    public void TestSpaceGreaterThan()
+    {
+        // ---------
+        // Arrange
+        // ---------
+        int checkIntervalEverySeconds = 3;
+        int toleranceSec = 1;
+        Folder folder = Common.CreateRootFolder();
+
+        DiskSpaceEventConfig config = new()
+        {
+            Id = 1,
+            Name = "Disk space event 1",
+            CheckIntervalSeconds = checkIntervalEverySeconds
+        };
+
+        DriveInfo? driveC = DriveInfo.GetDrives().Where(D => D.Name == @"C:\").FirstOrDefault();
+        Assert.IsNotNull(driveC, "Drive C: doesn't exist");
+
+        int diskFreeSpaceToCheckGB = (int)(((double)driveC.AvailableFreeSpace) / 1024 / 1024 / 1024);
+        diskFreeSpaceToCheckGB /= (int)2d;
+
+        DiskThreshold dt = new()
+        {
+            Disk = @"C:\",
+            CheckOperator = CheckOperator.GreaterThan,
+            ThresholdValue = diskFreeSpaceToCheckGB,
+            UnitMeasure = DiskThresholdUnitMeasure.Gigabytes
+        };
+
+        config.DiskThresholds.Add(dt);
+
+        DiskSpaceEvent eventObj = new()
+        {
+            ParentFolder = folder
+        };
+
+        object objSync = new();
+        ManualResetEvent mre = new(false);
+        bool eventTriggered = false;
+        eventObj.EventTriggered += (sender, e) =>
+        {
+            lock (objSync)
+            {
+                eventTriggered = true;
+            }
+
+            mre.Set();
+        };
+        eventObj.Config = config;
+
+        // ---------
+        // Act
+        // ---------
+        eventObj.Init();
+
+        mre.WaitOne(new TimeSpan(0, 0, checkIntervalEverySeconds + toleranceSec));
+
+        // ---------
+        // Assert
+        // ---------
+        lock (objSync)
+        {
+            Assert.IsTrue(eventTriggered);
+        }
+    }
+}
